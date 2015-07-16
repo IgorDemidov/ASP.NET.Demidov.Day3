4727e6a9ac0583cb83752cbe539f7312a0c84222
IgorDemidov <igr.demidov@gmail.com>
2015-07-17 00:43:35 +0300

    1step

---
+++ Polynom.Task1.Library/SinglePolinomial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynom.Task1.Library
+{
+    public sealed class SinglePolinomial : ICloneable, IEquatable<SinglePolinomial>
+    {
+        #region Properties
+        public int[] Сoefficients { get; private set; }
+
+        public int Degree { get; private set; }
+
+        #endregion
+        
+        #region Constructors
+
+        public SinglePolinomial(int[] coefficientsArray)
+        {
+            Сoefficients = coefficientsArray;
+            Degree = coefficientsArray.Length - 1;
+        }
+
+        public SinglePolinomial()
+            : this(new int[1] { 0 })
+        {
+        }
+
+        #endregion
+        
+        #region Interfaces implementation
+
+        #region ICloneable
+        public object Clone()
+        {
+            return new SinglePolinomial(Сoefficients);
+        }
+        #endregion
+
+        #region IEquatable<SinglePolinomial>
+
+        public bool Equals(SinglePolinomial other)
+        {
+            return this.Equals(other);       
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = Degree.GetHashCode();
+
+            foreach (int c in Сoefficients)
+            {
+                hashCode = hashCode ^ c;
+                
+            }
+            return hashCode;
+        }
+
+
+        #endregion
+
+        #endregion
+
+
+
+
+
+
+
+    }
+}
